e88f719a415853aa73ec136de02fc1401d28f4cb
Siddharthan <siddharthansp07@gmail.com>
2026-06-22 17:39:34 +0530

    Added panel (preview panel)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  return <div>test</div>
+export default function App()
+{
+  return (
+    <div style={page_style}>
+      <div style={panel_wrapper_style}>
+        <div style={panel_style}>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+const page_style = 
+{
+  display: 'flex',
+  width: '90vw',
+  height: '90vh',
+  overflow: 'hidden',
+};
+
+const panel_wrapper_style = 
+{
+  padding: '20px',
+  boxSizing: 'border-box',
+};
+
+const panel_style = 
+{
+  width: 'min(calc(90vh - 40px), 50vw)',
+  height: 'min(calc(90vh - 40px), 50vw)',
+  background: '#b88989',
+  border: '3px solid #000000',
+  borderRadius: '1px',
+};
